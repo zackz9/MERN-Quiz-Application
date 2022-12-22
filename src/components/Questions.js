3f80c3b92e9b1919b904f883cc1343d0fb00a5f3
zackz9 <zackel7593@gmail.com>
2022-12-22 20:20:38 +0100

    Get values from fetchQuestion Hook

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
 import data from '../database/data'
 // Call Custom hook
 import { useFetchQuestion } from '../hooks/FetchQuestion'
+
 export default function Questions() {
   const [checked, setChecked] = useState(undefined)
   //Destruct values of array
@@ -9,15 +11,24 @@ export default function Questions() {
 
   const question = data[0]
 
+  const questions = useSelector(
+    (state) => state.questions.queue[state.questions.trace]
+  )
+
+  const trace = useSelector(state => state.questions.trace)
+
   useEffect(() => {
-    // console.log(isLoading)
-    // console.log(apiData)
-    // console.log(serverError)
+    console.log(questions)
   })
 
   function onSelect() {
     // console.log('onSelect Radio')
   }
+
+  if (isLoading) return <h2 className='text-light'>isLoading</h2>
+  if (serverError)
+    return <h2 className='text-light'>serverError || "Unknown error"</h2>
+
   return (
     <div className='questions'>
       <h2 className='text-light'>{question.question}</h2>
